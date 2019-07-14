8496382cb25d39427bea0cd46019d1739bbf04f7
Luis <Luis@lm14-2240>
2019-07-14 14:41:47 -0400

    Lo mismo que el otro pero mejor porque el otro explotaba :3

---
+++ SistemaDealer1/Controllers/SucursalesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaDealer1.Models;
+
+namespace SistemaDealer1.Controllers
+{
+    public class SucursalesController : Controller
+    {
+        private SistemaDealer1DBContext db = new SistemaDealer1DBContext();
+
+        // GET: Sucursales
+        public ActionResult Index()
+        {
+            var sucursals = db.Sucursals.Include(s => s.Encargado);
+            return View(sucursals.ToList());
+        }
+
+        // GET: Sucursales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursals.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // GET: Sucursales/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmpleadoId = new SelectList(db.Empleados, "EmpleadoId", "Usuario");
+            return View();
+        }
+
+        // POST: Sucursales/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sucursals.Add(sucursal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmpleadoId = new SelectList(db.Empleados, "EmpleadoId", "Usuario", sucursal.EmpleadoId);
+            return View(sucursal);
+        }
+
+        // GET: Sucursales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursals.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmpleadoId = new SelectList(db.Empleados, "EmpleadoId", "Usuario", sucursal.EmpleadoId);
+            return View(sucursal);
+        }
+
+        // POST: Sucursales/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sucursal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmpleadoId = new SelectList(db.Empleados, "EmpleadoId", "Usuario", sucursal.EmpleadoId);
+            return View(sucursal);
+        }
+
+        // GET: Sucursales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = db.Sucursals.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: Sucursales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sucursal sucursal = db.Sucursals.Find(id);
+            db.Sucursals.Remove(sucursal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
