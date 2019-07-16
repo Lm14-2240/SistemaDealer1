2256c3092d0eba1563167f8afc2e920fbd342240
Pedro Lebron <plebron@CARDNET.COM.DO>
2019-07-15 23:41:30 -0400

    Se agrega Controlador de JavaScript con las acciones necesarias para realizar el filtrado

---
+++ SistemaDealer1/Scripts/VehiculoController.js
@@ -0,0 +1,11 @@
+﻿$("#MarcaId").change(function () {
+    var marcaId = $('#MarcaId').val();
+    
+    $.get("/api/ModeloAPI?marcaId=" + marcaId, function (response) {
+        $("#ModeloId").html(" ");
+
+        $.each(response, function (key, value) {
+            $('#ModeloId').html($("#ModeloId").html() + "<option value=" + value.ModeloId + " > " + value.Descripcion + "</option > ");
+        });
+    });
+});
