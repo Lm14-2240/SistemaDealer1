bb209ab35b42e7b4755cceaa702c3ae59fc6afa7
Pedro Lebron <plebron@CARDNET.COM.DO>
2019-08-12 13:24:18 -0400

    Correccion en la ruta para ir primero al Login

--- SistemaDealer1/App_Start/RouteConfig.cs
+++ SistemaDealer1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SistemaDealer1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Vehiculoes", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
